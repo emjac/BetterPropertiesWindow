7fd2623bfdb23787034648187d7f11d8502637f3
Emily <emily.jac@gmail.com>
2018-08-07 13:49:48 -0400

    test

--- BetterPropertiesWindow/Form1.Designer.vb
+++ BetterPropertiesWindow/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,112 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.ButtonBump = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(414, 588)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(322, 54)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Assign To Selected Beams"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(42, 588)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(290, 29)
+        Me.CheckBox1.TabIndex = 6
+        Me.CheckBox1.Text = "Highlight Selected Beams"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.AllowUserToResizeRows = False
+        Me.DataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells
+        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight
+        Me.DataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersVisible = False
+        Me.DataGridView1.RowTemplate.Height = 33
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(724, 559)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'ButtonBump
+        '
+        Me.ButtonBump.Location = New System.Drawing.Point(450, 810)
+        Me.ButtonBump.Name = "ButtonBump"
+        Me.ButtonBump.Size = New System.Drawing.Size(250, 54)
+        Me.ButtonBump.TabIndex = 11
+        Me.ButtonBump.Text = "Bump Sections"
+        Me.ButtonBump.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(16, 724)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(316, 42)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "Copy Node Numbers As Table"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(16, 785)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(316, 42)
+        Me.Button4.TabIndex = 13
+        Me.Button4.Text = "Copy Beam Numbers As Table"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
-    End Sub
+        Me.ClientSize = New System.Drawing.Size(788, 923)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ButtonBump)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Awesome Staad Helper"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
 
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents CheckBox7 As CheckBox
+    Friend WithEvents CheckBox6 As CheckBox
+    Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents ButtonBump As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button4 As Button
 End Class
--- BetterPropertiesWindow/Form1.vb
+++ BetterPropertiesWindow/Form1.vb
@@ -1,3 +1,204 @@
-﻿Public Class Form1
+﻿
+Public Class Form1
 
+    Dim std As Object = GetObject(, "StaadPro.OpenSTAAD")
+
+    Dim prop As OpenSTAADUI.OSPropertyUI = std.Property
+    Dim geo As OpenSTAADUI.OSGeometryUI = std.Geometry
+
+
+    Dim lSectionRefs As Long
+    Dim bSec As Boolean
+
+    Private Sub Form1_Activated(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Activated
+
+        Me.TopMost = True
+
+        LoadDataIntoDataGridView()
+
+    End Sub
+
+    Private Sub LoadDataIntoDataGridView()
+
+        With DataGridView1
+
+            Dim dProp = New DataTable
+            Dim dRow As DataRow
+            Dim lBeamList() As Integer
+            Dim bSec As Boolean
+            Dim width As Double
+            Dim depth As Double
+            Dim Ix As Double
+            Dim Iy As Double
+            Dim Iz As Double
+            Dim Ax As Double
+            Dim Ay As Double
+            Dim Az As Double
+            Dim lSectionCnt As Long = prop.GetSectionPropertyCount()
+
+            .Width = 380
+
+            dProp.Columns.Add("Ref")
+            dProp.Columns.Add("Section")
+            dProp.Columns.Add("Material")
+            dProp.Columns.Add("Iz")
+            dProp.Columns.Add("Zz")
+
+            For lSec As Long = 1 To lSectionCnt
+                dRow = dProp.NewRow()
+                dRow("Ref") = lSec
+                dRow("Section") = "s"
+
+                prop.GetSectionPropertyName(lSec, dRow("Section"))
+                prop.GetSectionPropertyAssignedBeamCount(lSec)
+
+                ReDim lBeamList(0 To prop.GetSectionPropertyAssignedBeamCount(lSec) - 1)
+
+                If Not lBeamList.Length = 0 Then
+                    bSec = prop.GetSectionPropertyAssignedBeamList(lSec, lBeamList)
+                    dRow("Material") = prop.GetBeamMaterialName(lBeamList(0))
+                    dRow("Iz") = prop.GetBeamProperty(lBeamList(0), width, depth, Ax, Ay, Az, Ix, Iy, Iz)
+                    If Not Ax = 0 Then
+                        Dim Zz As Long = Math.Round(Iz / Az)
+                        dRow("Iz") = Iz
+                        dRow("Zz") = Zz
+                    End If
+                End If
+
+                dProp.Rows.Add(dRow)
+
+            Next
+
+            .DataSource = dProp
+
+        End With
+
+        Dim colRef As DataGridViewColumn = DataGridView1.Columns(0)
+        Dim colSec As DataGridViewColumn = DataGridView1.Columns(1)
+        Dim colMat As DataGridViewColumn = DataGridView1.Columns(2)
+        Dim colIz As DataGridViewColumn = DataGridView1.Columns(3)
+        Dim colZz As DataGridViewColumn = DataGridView1.Columns(4)
+
+        colRef.Width = 40
+        colSec.Width = 125
+        colMat.Width = 80
+        colIz.Width = 50
+        colZz.Width = 50
+
+        bSec = prop.GetSectionPropertyList(lSectionRefs)
+    End Sub
+
+    Private Sub DataGridView(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+
+        If Not e.RowIndex = -1 Then
+
+            Dim oValue As Object = DataGridView1.Rows(e.RowIndex).Cells(0).Value
+
+            Dim iChk As Boolean = CheckBox1.Checked
+
+            If iChk Then
+                geo.ClearMemberSelection()
+                Dim lBeamList() As Integer
+                prop.GetSectionPropertyAssignedBeamCount(oValue)
+                ReDim lBeamList(0 To prop.GetSectionPropertyAssignedBeamCount(oValue) - 1)
+                bSec = prop.GetSectionPropertyAssignedBeamList(oValue, lBeamList)
+                geo.SelectMultipleBeams(lBeamList)
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'comment
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim lRef As Long
+        Dim SelBeamsCnt As Long
+
+        SelBeamsCnt = geo.GetNoOfSelectedBeams
+
+        Dim SelBeams(SelBeamsCnt - 1) As Integer
+        geo.GetSelectedBeams(SelBeams, 1)
+
+        lRef = DataGridView1.CurrentRow.Cells(0).Value
+
+        prop.AssignBeamProperty(SelBeams, lRef)
+
+        LoadDataIntoDataGridView()
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub BeamBumper()
+
+        Dim i As Integer
+        Dim SelBeamsNo As Long
+        Dim SelBeams() As Integer
+        Dim Country As Long
+        Dim SectionName As String
+        Dim SectionNameBumped As String
+        Dim TypeSpec As Long
+        Dim Ref As Long
+        Dim Mat As String
+        Dim dSectionWeight As Double
+        Dim sSectionWeight As Double
+        Dim X_Pos As Double
+        Dim BumpChk As Boolean
+        Dim SectionClass As String
+
+        Country = 1 ' USA USA!
+        TypeSpec = 0
+
+        SelBeamsNo = geo.GetNoOfSelectedBeams
+
+        If SelBeamsNo > 0 Then
+            ReDim SelBeams(SelBeamsNo - 1)
+
+            geo.GetSelectedBeams(SelBeams, 1)
+
+            For i = 0 To SelBeamsNo - 1
+
+                SectionName = prop.GetBeamSectionName(SelBeams(i))
+
+                Dim ChkW As Boolean = IIf(Strings.Left(SectionName, 1) = "W", True, False)
+
+                If ChkW Then 'Zero for ST  Sections
+                    BumpChk = 0
+
+                    X_Pos = InStr(SectionName, "X")
+                    sSectionWeight = Strings.Right(SectionName, Strings.Len(SectionName) - X_Pos)
+                    dSectionWeight = Convert.ToDouble(sSectionWeight)
+                    SectionClass = Strings.Left(SectionName, X_Pos)
+                    Mat = prop.GetBeamMaterialName(SelBeams(i))
+
+                    While Not BumpChk
+
+                        dSectionWeight = dSectionWeight + 1
+                        SectionNameBumped = Trim(SectionClass & CStr(dSectionWeight))
+                        Ref = prop.CreateBeamPropertyFromTable(Country, SectionNameBumped, TypeSpec, 0.0, 0.0)
+                        BumpChk = prop.AssignBeamProperty(SelBeams(i), Ref)
+                        prop.AssignMaterialToMember(Mat, SelBeams(i))
+
+                    End While
+                End If
+
+
+            Next i
+
+        End If
+
+        LoadDataIntoDataGridView()
+
+    End Sub
+
+    Private Sub ButtonBump_Click(sender As Object, e As EventArgs) Handles ButtonBump.Click
+        BeamBumper()
+    End Sub
 End Class
+
