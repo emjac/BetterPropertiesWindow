8f5eadfd2e5bad5d612704a88b826ecdc29958c3
Emily <emily.jac@gmail.com>
2018-10-16 17:59:27 -0400

    Adding Excel Database

--- BetterPropertiesWindow/Form1.vb
+++ BetterPropertiesWindow/Form1.vb
@@ -348,9 +348,50 @@ Public Class Form1
 
     End Sub
 
+    Private Sub GetAISCDataBase()
 
+        Dim sSheetName As String = ""
+        Dim sConnection As String
+        Dim dtTablesList As DataTable
+        Dim oleExcelCommand As OleDbCommand
+        Dim oleExcelReader As OleDbDataReader
+        Dim oleExcelConnection As OleDbConnection
 
+        sConnection = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\C:\Users\ejacobsen\Google Drive\LeM\Spreadsheets\Templatesaisc-shapes-database-v15.0.xlsx;Extended Properties=""Excel 12.0;HDR=No;IMEX=1"""
 
+        oleExcelConnection = New OleDbConnection(sConnection)
+        oleExcelConnection.Open()
+
+        dtTablesList = oleExcelConnection.GetSchema("Tables")
+
+        If dtTablesList.Rows.Count > 0 Then
+            sSheetName = dtTablesList.Rows(0)("TABLE_NAME").ToString
+        End If
+
+        dtTablesList.Clear()
+        dtTablesList.Dispose()
+
+        If sSheetName <> "" Then
+
+            oleExcelCommand = oleExcelConnection.CreateCommand()
+            oleExcelCommand.CommandText = "Select * From [" & sSheetName & "]"
+            oleExcelCommand.CommandType = CommandType.Text
+
+            oleExcelReader = oleExcelCommand.ExecuteReader
+
+            Dim nOutputRow As Double = 0
+
+            While oleExcelReader.Read
+
+            End While
+
+            oleExcelReader.Close()
+
+        End If
+
+        oleExcelConnection.Close()
+
+    End Sub
 
     Private Sub BeamDumper()
 
